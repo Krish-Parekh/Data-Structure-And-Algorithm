86467967157fede09c56d6086e8c4daa95f4ff5a
Krish Parekh <Krishp4123@gmail.com>
2021-09-01 18:12:05 +0530

    initial commit

--- Array/SingleNumber.cpp
+++ Array/SingleNumber.cpp
@@ -16,13 +16,30 @@
 using namespace std;
 
 /*
+    Solution : 
+    Time Complexity : O(N)
+    Space Complexity : O(1)
 
-Debug:
+    Explanation :
+    1. We use logical operator to solve this question
+    2. As we have to find one unique element between duplicate element
+    3. When we xor number with itself it outputs zero
+        -> 1^1 = 0 , 0^0 = 0
+        -> 1^0 = 1 , 0^1 = 0
+    
+    Debug: 
+    nums = [2,2,1]
+    unique = 0
+    when i = 0:
+        unique = 0 ^ 2 // 2
+    
+    when i = 1:
+        unique = 2 ^ 2 // 0
 
+    when i = 2:
+        unique = 0 ^ 1 // 1
 
-
-
-
+    So our unique element is 1
 */
 
 class Solution {
