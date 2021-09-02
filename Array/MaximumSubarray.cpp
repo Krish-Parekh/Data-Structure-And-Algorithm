bded709dc6d41c539bb350585feea71e735b9c77
Dharmik Pansuriya <76813316+DharmikPansuriya@users.noreply.github.com>
2021-09-02 17:56:55 +0530

    Update MaximumSubarray.cpp

--- Array/MaximumSubarray.cpp
+++ Array/MaximumSubarray.cpp
@@ -41,36 +41,18 @@ public:
     }
 };
 
-    /*  
-        When int is too large and we have to use long long int.
-        
-        Solution:
-        Time Complexity : O(N)
-        Space Complexit : O(1)
-        
-        
-        1. We will use kaden's algortirm to solve this question
-        2. We take two variable 
-            mx -> Total Maximum Sum
-            sum -> Max So Far 
-        3. We add every element in array with sum
-        4. Then we check for 
-            if mx(Maximum Sum) < sum(Max So Far)
-             yes -> Update mx(Maximum sum) 
-             No  -> Continue in loop
-        5. Now Check 
-                if Sum < 0 
-                    yes -> Update Sum to 0 
-            This condition is used because if Sum is less then 0 which means negative so 
-            its no use to carry forward this Sum because we have to find Maimum Subarray sum 
-    */
 
+
+/*  
+    When int is too large and we have to use long long int.
+    Explanation, TC, SC are same as above approach.
+*/
 
 class Solution{
     public:
     // arr: input array
     // n: size of array
-    //Function to find the sum of contiguous subarray with maximum sum.
+    // Function to find the sum of contiguous subarray with maximum sum.
     long long maxSubarraySum(int arr[], int n){
         
         long long int mx = INT_MIN;
@@ -80,6 +62,8 @@ class Solution{
             if(sum > mx){
                 mx = sum;
             }
+            
+    // Program will change from here for long long int
             if(sum < 0){
                 sum = 0;
             }
