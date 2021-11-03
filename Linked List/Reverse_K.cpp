6503ea28c721d3053a3a123f0784516b0693ed46
Krish Parekh <Krishp4123@gmail.com>
2021-11-03 18:19:17 +0530

    Reverse K nodes in Linked List

---
+++ Linked List/Reverse_K.cpp
@@ -0,0 +1,32 @@
+#include "BaseClass.cpp"
+node *reverseK(node * head , int k)
+{
+    int count = 0;
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr = NULL;
+    while (currptr != NULL and count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+
+    if (nextptr != NULL)
+    {
+        head->next = reverseK(nextptr, k);
+    }
+    return prevptr;
+}
+
+int main()
+{
+    node * head = takeInput();
+    int kSize;
+    cout << "Enter Size of K : " << " ";
+    cin >> kSize;
+    node * res = reverseK(head,kSize);
+    printLL(res);
+}
