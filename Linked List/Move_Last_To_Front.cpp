d4c1c64fd67cf3ab7cdf8f97fd16c37b1af84698
Krish Parekh <Krishp4123@gmail.com>
2021-11-09 17:19:05 +0530

    Move Last To Front

---
+++ Linked List/Move_Last_To_Front.cpp
@@ -0,0 +1,24 @@
+#include "BaseClass.cpp"
+
+node * moveLast(node * head)
+{
+    node* temp = head;
+    node* prev = NULL;
+    while(temp->next)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    temp->next = head;
+    head = temp;
+    return temp;
+
+}
+
+int main()
+{
+    node * head = takeInput();
+    node * res = moveLast(head);
+    printLL(res);
+}
